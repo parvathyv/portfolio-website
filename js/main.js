37a93efbe763f0956e81ac324c36d1d62889e991
Parvathy <parvathyv@yahoo.com>
2020-07-04 10:57:36 -0500

    Changed order of media query to make hamburger menu work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 /* Toggle between adding and removing the "responsive" class to menu when the user clicks on the icon */
-
-
 function myFunction() {
+   
   var x = document.getElementById("myTopmenu");
+   
   if (x.className === "menu") {
     x.className += " responsive";
   } else {
     x.className = "menu";
   }
+
 }
 
+/*$('.myDiv').click(function(e)
+{
+    $(this).toggleClass('fullscreen');
+
+});*/
