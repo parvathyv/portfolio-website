a12c08a9ee31c51971d37babd6e1f978c5e35400
Parvathy <parvathyv@yahoo.com>
2020-07-17 11:27:27 -0500

    closing secondary menu automatically

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to menu when the user clicks on the icon */
+var xpos;
+var ypos;
+
 function myFunction() {
    
   var x = document.getElementById("myTopmenu");
@@ -18,10 +21,26 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+/*setTimeout( function closeNavTimer() {
+  document.getElementById("mySidenav").style.width = "0";
+}, 3000);*/
+
+
+var el = document.getElementById("process_button");
+el.addEventListener("click", function() {setTimeout(closeNavTimer() , 200)});
+
+function closeNavTimer() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+/*window.addEventListener("scroll", function(event) {
+        ypos = this.scrollY;
+        xpos =this.scrollX;
+     	
+}, false);*/
+
+
+
 console.log("Hello. Welcome to my UX/UI portfolio website!")
-
-/*$('.myDiv').click(function(e)
-{
-    $(this).toggleClass('fullscreen');
-
-});*/
