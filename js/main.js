8ebe7a8b31d98e61c745200ffe98c30c773c2227
Parvathy <parvathyv@yahoo.com>
2020-07-22 13:41:32 -0500

    Changed project links to same window in index

--- js/main.js
+++ js/main.js
@@ -3,36 +3,37 @@
 
 function myFunction() {
    
-  var x = document.getElementById("myTopmenu");
+    var x = document.getElementById("myTopmenu");
   
-
-  if(window.location.href.includes("index.html"))
-  {
+    // To remove a menu element for Mobile
+    if(window.location.href.includes("index.html")){
        if(document.getElementById("index_click")){
-        var el=document.getElementById("index_click");
-        el.remove();
-      }
-  }else{
-    if(window.location.href.includes("about.html")){
-        if(document.getElementById("about_click")){
-          var el=document.getElementById("about_click");
-          el.remove();
-      }
+            var el=document.getElementById("index_click");
+            el.remove();
+        } 
     }else{
-      if(window.location.href.includes("fitted.html")){
-         if(document.getElementById("work_click")){
-           var el=document.getElementById("work_click");
-          el.remove();
-      }
-      }
+        
+        if(window.location.href.includes("about.html")){
+            if(document.getElementById("about_click")){
+                var el=document.getElementById("about_click");
+                el.remove();
+            }
+        }else{
+          
+            if(window.location.href.includes("fitted.html")){
+                if(document.getElementById("work_click")){
+                    var el=document.getElementById("work_click");
+                    el.remove();
+                }
+            }
+        }
     }
-  }
 
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
 
 }
 
@@ -41,38 +42,39 @@ function myFunction() {
 }*/
 function openSidebar(x) {
 
-  if (x.matches) { // If media query matches
-    document.getElementById("mySidenav").style.width = "150px";
-  } else {
-      document.getElementById("mySidenav").style.width = "250px";
-  }
+    if (x.matches) { 
+        document.getElementById("mySidenav").style.width = "150px";
+    } else {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
 }
 
 
 
 function openNav(){
-  var x = window.matchMedia("(max-width: 500px)");
-  if(document.getElementById("menubutton")||document.getElementById("menubutton_vela")){
-    openSidebar(x);
-  }
+
+    var x = window.matchMedia("(max-width: 500px)");
+    if(document.getElementById("menubutton")||document.getElementById("menubutton_vela")){
+        openSidebar(x);
+    }
 };
 
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 
 var els = document.getElementsByClassName("menu_button");
 
 for(var i = 0; i < els.length; i++) {
-            var el = els[i];
-            el.addEventListener("click", function() {setTimeout(closeNavTimer() , 200)});
-        }
+    var el = els[i];
+    el.addEventListener("click", function() {setTimeout(closeNavTimer() , 200)});
+}
 
 
 function closeNavTimer() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 
@@ -82,41 +84,41 @@ function closeNavTimer() {
      	
 }, false);*/
 
-// Select all links with hashes
+// Select all links with hashes, smooth scroll
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname) 
+    {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // Does a scroll target exist?
-      if (target.length) {
+        if (target.length) {
         // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
         });
       }
     }
-  });
+});
 
 console.log("Hello. Welcome to my UX/UI portfolio website!")
