f39979260c41ce223804ce70371ac2384d21887e
Parvathy <parvathyv@yahoo.com>
2020-08-25 13:57:23 -0500

    Updated Fitted Case Study

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ to menu when the user clicks on the icon */
 
 var y = window.matchMedia("(min-width: 1800px)");
 
-if (window.location.href.includes("index.html") || window.location.href == "https://parvathyv.github.io/portfolio-website/") 
+if (window.location.href.includes("index.html") || window.location.href == "https://parvathyv.github.io/portfolio-website/")
 {
 
     var my_illus = document.getElementById("my_illus");
